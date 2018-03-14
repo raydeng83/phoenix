570e4efc1e18dd231540be56e18edd1f12f332d9
Le Deng <ldeng@ikaSystems.com>
2018-03-14 11:57:09 -0400

    add backend session tracker

--- phoenix-frontend/src/app/app.routing.ts
+++ phoenix-frontend/src/app/app.routing.ts
@@ -3,7 +3,7 @@ import {Routes, RouterModule} from '@angular/router';
 
 import {LoginComponent} from './components/login/login.component';
 import {RegistrationComponent} from './components/registration/registration.component';
-import {HomeComponent} from './components/Home/home.component';
+import {HomeComponent} from './components/home/home.component';
 
 
 const appRoutes: Routes = [
